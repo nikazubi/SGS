9efa3ddaa2cd8913b43892c4775678a85f26fdb6
Nikolozi Zubiashvili <nikolozi2@discovery.co.za>
2023-10-20 04:11:07 +0400

    asdnasd

---
+++ admin-console/src/front-ac/src/main/pages/semesterPage/useUpdateSemesterDiagnosticGrade.js
@@ -0,0 +1,27 @@
+import axios from "../../../utils/axios";
+import useMutationWithInvalidation from "../../../hooks/useMutationWithInvalidation";
+
+
+export const updateSemesterDiagnosticGrade = async grade => {
+    let gradeType = Object.keys(grade).filter(field => field.endsWith("--4") || field.endsWith("--3"))[0];
+    if (gradeType.endsWith('--3')) {
+        gradeType = 'DIAGNOSTICS_1'
+    } else if (gradeType.endsWith('--4')) {
+        gradeType = 'DIAGNOSTICS_2'
+    }
+    console.log("gradee", grade)
+    const request = {
+        // id: grade.grades.filter(g => g.gradeType === gradeType)[0].id,
+        value: grade.value,
+        gradeType: gradeType,
+        student: grade.student,
+        subject: grade.subject,
+        exactMonth: grade.exactMonth
+    }
+    const {data} = await axios.post("/grade/insert-student-grade", request);
+    return data;
+};
+
+const useUpdateSemesterDiagnosticGrade = () => useMutationWithInvalidation(updateSemesterDiagnosticGrade, "SEMESTER_GRADE");
+
+export default useUpdateSemesterDiagnosticGrade
