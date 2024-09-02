ec1dce27f2023627029cff7cb8a40954510eb44e
AnaKentchuashvili <ana.kentchuashvili@gmail.com>
2024-09-03 00:54:08 +0400

    added datagrid on year client side

--- client-console/src/pages/TsliuriShefaseba/index.js
+++ client-console/src/pages/TsliuriShefaseba/index.js
@@ -2,10 +2,12 @@ import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
-import React, {useCallback, useEffect, useMemo, useState} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 import CustomShefasebaBar from "../../components/CustomShefasebaBar";
 import useFetchYear from "../semestruli-shefaseba/useYear";
 import useGradeAnual from "./useGradeAnual";
+import DataGridSGS from "../../components/datagrid/DataGrid";
+import DataGridPaper from "../../components/datagrid/DataGridPaper";
 
 const TsliuriShefaseba = () => {
 
@@ -60,59 +62,201 @@ const TsliuriShefaseba = () => {
         },
         [yearData, chosenYear, handleChange],
     );
-    
-    
-        const handleSearch = async () =>{
-            if(!!selectedData) {
-                const studentYearlyGrades = [
-                    {
-                        name: 'ქართული ენა და ლიტერატურა',
-                        ქულა: 7,
-                    },
 
-                    {
-                        name: 'მათემატიკა',
-                        ქულა: 6,
-                    },
 
-                    {
-                        name: 'ინგლისური',
-                        ქულა: 7,
-                    },
-                    {
-                        name: 'ისტორია',
-                        ქულა: 6,
-                    },
-                    {
-                        name: 'გეოგრაფია',
-                        ქულა: 7,
-                    }]
-                
-                setCurrentData(studentYearlyGrades)
-            }
+    const handleSearch = async () => {
+        if (!!selectedData) {
+            const studentYearlyGrades = [
+                {
+                    name: 'ქართული ენა და ლიტერატურა',
+                    ქულა: 7,
+                },
+
+                {
+                    name: 'მათემატიკა',
+                    ქულა: 6,
+                },
+
+                {
+                    name: 'ინგლისური',
+                    ქულა: 7,
+                },
+                {
+                    name: 'ისტორია',
+                    ქულა: 6,
+                },
+                {
+                    name: 'გეოგრაფია',
+                    ქულა: 7,
+                }]
+
+            setCurrentData(studentYearlyGrades)
         }
-    
-    
-        return ( 
-            <>
-                <div className="ib__center column">
-                    <div className="pageName">მოსწავლის წლიური შეფასება</div>
-                    <div style={{display: 'flex', alignItems: 'center', marginTop: '25px'}}>
-                        {getYearDropdown()}
-                        <div style={{marginLeft: '10px'}}>
-                            <Button onClick={handleSearch} disabled={!selectedData}
-                                    style={{fontWeight: 'bold', height: '50px'}} variant="contained">ძიება<SearchIcon/></Button>
-                        </div>
+    }
+
+    let gradeClomuns3 = []
+
+    const getFieldName = (o, num) => {
+        return o.subject.id + "-" + num;
+    }
+
+    function customRound(value) {
+        const res = value % 1 >= 0.5 ? Math.ceil(value) : Math.floor(value);
+        return res === 0 ? "" : res
+    }
+
+    const gradeColumns = [
+        {
+            headerName: "საგანი",
+            renderCell: ({row}) => {
+                return (<div>
+                    {row.subject.name}
+                </div>);
+            },
+            renderHeader: (params) => (
+                <div style={{height: 150, textAlign: 'center', fontSize: 16}}>
+                    {'საგანი'}
+                </div>
+            ),
+            field: 'name',
+            align: 'center',
+            headerAlign: 'center',
+        },
+        {
+            headerName: "სემესტრი I",
+            renderCell: ({row}) => {
+                return (<div>
+                    {row.value["1"] === 0 ? "" : row.value["1"]}
+                </div>);
+            },
+            renderHeader: (params) => (
+                <div style={{height: 150, textAlign: 'center', fontSize: 16}}>
+                    {"სემესტრი I"}
+                </div>
+            ),
+            field: 'semester1',
+            // sortable: false,
+            align: 'center',
+            headerAlign: 'center',
+        },
+        {
+            headerName: "სემესტრი II",
+            renderCell: ({row}) => {
+                return (<div>
+                    {row.value["2"] === 0 ? "" : row.value["2"]}
+                </div>);
+            },
+            renderHeader: (params) => (
+                <div style={{height: 150, textAlign: 'center', fontSize: 16}}>
+                    {"სემესტრი II"}
+                </div>
+            ),
+            field: 'semester2',
+            // sortable: false,
+            align: 'center',
+            headerAlign: 'center',
+        },
+        {
+            headerName: "წლიური ქულა",
+            renderCell: ({row}) => {
+                return (<div>
+                    {customRound((Number(row.value["1"]) + Number(row.value["2"])) / 2)}
+                </div>);
+            },
+            renderHeader: (params) => (
+                <div style={{height: 150, textAlign: 'center', fontSize: 16}}>
+                    {"წლიური ქულა"}
+                </div>
+            ),
+            field: 'yearly',
+            // sortable: false,
+            align: 'center',
+            headerAlign: 'center',
+        },
+        {
+            headerName: "გამოცდა",
+            renderCell: ({row}) => {
+                return (<div>
+                    {row.value["3"] === 0 ? "" : row.value["3"]}
+                </div>);
+            },
+            renderHeader: (params) => (
+                <div style={{height: 150, textAlign: 'center', fontSize: 16}}>
+                    {"გამოცდა"}
+                </div>
+            ),
+            field: 'exam',
+            // sortable: false,
+            align: 'center',
+            headerAlign: 'center',
+        },
+        {
+            headerName: "საბოლოო ქულა",
+            renderCell: ({row}) => {
+                return (<div>
+                    {row.value["4"] === 0 ? "" : row.value["4"]}
+                </div>);
+            },
+            renderHeader: (params) => (
+                <div style={{height: 150, textAlign: 'center', fontSize: 16}}>
+                    {"საბოლოო ქულა"}
+                </div>
+            ),
+            field: 'final',
+            // sortable: false,
+            align: 'center',
+            headerAlign: 'center',
+        },
+    ];
+
+    //
+    // const gradeColumns = useMemo(() => {
+    //     return
+    // }, [])
+
+
+    return (
+        <>
+            <div className="ib__center column">
+                <div className="pageName">მოსწავლის წლიური შეფასება</div>
+                <div style={{display: 'flex', alignItems: 'center', marginTop: '25px'}}>
+                    {getYearDropdown()}
+                    <div style={{marginLeft: '10px'}}>
+                        <Button onClick={handleSearch} disabled={!selectedData}
+                                style={{fontWeight: 'bold', height: '50px'}}
+                                variant="contained">ძიება<SearchIcon/></Button>
                     </div>
                 </div>
+            </div>
 
-                {!!parsedData && parsedData.length > 0 && <div style={{marginTop: 30}}>
-                    <CustomShefasebaBar color={'#45c1a4'} data={parsedData} height={450} width={window.innerWidth - 30}
-                                        left={0}/>
-                </div>}
+            <div>
+                {/*<SemesterGradeToolbar filters={filters} setFilters={setFilters} checked={checked} setChecked={setChecked}/>*/}
+                <div style={{height: `45vh`, width: '100%', marginTop: 30, paddingLeft: 40, paddingRight: 40}}>
+                    <DataGridPaper>
+                        <DataGridSGS
+                            queryKey={"ANNUAL_GRADE"}
+                            // experimentalFeatures={{columnGrouping: true}}
+                            // columnGroupingModel={getGradeColumns()}
+                            columns={gradeColumns}
+                            rows={data ? data[0]?.gradeList : []}
+                            getRowId={(row) => {
+                                return row.subject.id;
+                            }}
+                            getRowHeight={() => 50}
+                            disableColumnMenu
+                            // filters={filters}
+                        />
+                    </DataGridPaper>
+                </div>
+            </div>
 
-            </>
-         );
+            {!!parsedData && parsedData.length > 0 && <div style={{marginTop: 30}}>
+                <CustomShefasebaBar color={'#45c1a4'} data={parsedData} height={450} width={window.innerWidth - 30}
+                                    left={0}/>
+            </div>}
+
+        </>
+    );
 }
- 
+
 export default TsliuriShefaseba;
--- client-console/src/pages/TsliuriShefaseba/useGradeAnual.js
+++ client-console/src/pages/TsliuriShefaseba/useGradeAnual.js
@@ -7,7 +7,9 @@ export const fetchGradesAnual = async (filters) => {
         yearRange: filters && filters.yearRange ? filters.yearRange[0].toString() : null,
     }
     const {data} = await axios.get("/client/grade/get-grades-year", {params});
-    return data;
+    const result = data ? data[0] : {gradeList: []};
+    const bla = result.gradeList.filter(a => a.subject.id !== 8888 && a.subject.id !== 9999)
+    return [{student: data.student, gradeList: bla}];
 }
 
 const useGradeAnual = (filterData) => useQuery(["ANNUAL_GRADE", filterData],
